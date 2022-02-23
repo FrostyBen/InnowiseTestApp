17ea77e40402fa7ad05fa3dadd95d917885726e0
FrostyBen <frostiben@gmail.com>
2022-02-23 01:27:41 +0100

    finish weather screen

--- lib/app/ui/screens/today_screen.dart
+++ lib/app/ui/screens/today_screen.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/bloc/weather_bloc/weatherbloc_bloc.dart';
 
 class TodayScreen extends StatelessWidget {
   const TodayScreen({Key? key}) : super(key: key);
+  
+String getDirection(int direction){
+  if(direction < 45 && direction >= 0){
+    return 'N';
+  }else if( direction < 90 && direction >= 45){
+    return 'NE';
+  } else if (direction < 135 && direction >= 90){
+    return 'E';
+  } else if( direction <= 180 && direction >= 135){
+    return 'SE';
+  } else if ( direction < 225 && direction >= 180){
+    return 'S';
+  } else if( direction <= 280 && direction >= 225){
+    return 'SW';
+  } else if( direction < 325 && direction >=280){
+    return 'W';
+  } else {
+    return 'NW';
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -9,114 +32,142 @@ class TodayScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Today'),
       ),
-      body: Center(
+      body: BlocBuilder<WeatherBloc, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherStateLoading){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          if (state is WeatherStateLoaded){
+
+          
+          return Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+                   Image.asset(
+                    'assets/${state.currentWeather.weather!.first.icon!}.png',
+                    height: 100,
+                    width: 100,
+                  ),
+                  const SizedBox(height: 10),
+                 Text(
+                    state.currentWeather.name!,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                   Text(
+                    (state.currentWeather.main!.temp! -273.15).toString() + ' °C' ,
+                    style: TextStyle(color: Colors.blue, fontSize: 35),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    indent: 100,
+                    endIndent: 100,
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: const Icon(Icons.cloud),
+                          ),
+                           Text(state.currentWeather.main!.humidity!.toString() + ' %')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: const Icon(Icons.opacity),
+                          ),
+                          const Text('1.0mm'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: const Icon(Icons.device_thermostat_outlined),
+                          ),
+                           Text(state.currentWeather.main!.pressure!.toString() + ' hPa')
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: const Icon(Icons.air),
+                          ),
+                           Text(state.currentWeather.wind!.speed!.toInt().toString() + ' km/h')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                            ),
+                            child: const Icon(Icons.explore),
+                          ),
+                           Text(getDirection(state.currentWeather.wind!.deg!))
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    indent: 100,
+                    endIndent: 100,
+                    height: 80,
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      context.read<WeatherBloc>().add(ShareWeatherEvent());
+                    },
+                    child: const Text(
+                      'Share',
+                      style: TextStyle(
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            ),
+          );
+          }
+          if( state is WeatherStateError){
+            return Center(child: Text('You have an error'),);
+          }
+          return Container();
+        },
         
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height:15),
-              const Icon(
-                Icons.wb_sunny_outlined,
-                size: 170,
-              ),
-              const SizedBox(
-                height: 10
-              ),
-              const Text('London,UK', style: TextStyle(fontSize: 20),),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                '22℃| Sunny',
-                style: TextStyle(color: Colors.blue, fontSize: 35),
-              ),
-              const Divider(
-                thickness: 2,
-                indent: 100,
-                endIndent: 100,
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1),
-                        ),
-                        child: const Icon(Icons.cloud),
-                      ),
-                      const Text('57%')
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1),
-                        ),
-                        child: const Icon(Icons.opacity),
-                      ),
-                      const Text('1.0mm'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1),
-                        ),
-                        child: const Icon(Icons.device_thermostat_outlined),
-                      ),
-                      const Text('1019 hPa')
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1),
-                        ),
-                        child: const Icon(Icons.air),
-                      ),
-                      const Text('20 km/h')
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 1),
-                        ),
-                        child: const Icon(Icons.explore),
-                      ),
-                      const Text('SE')
-                    ],
-                  ),
-                ],
-              ),
-              const Divider(
-                thickness: 2,
-                indent: 100,
-                endIndent: 100,
-                height: 80,
-              ),
-              const Text(
-                'Share',
-                style:
-                    TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              SizedBox(height: 20,)
-            ],
-          ),
-        ),
       ),
     );
   }
